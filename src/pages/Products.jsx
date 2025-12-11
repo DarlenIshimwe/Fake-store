406d3fc7e7fe0fbef3e0484e9b6616e396835adb
Darlene Ishimwe <darleneishimwe73@gmail.com>
2025-12-11 11:31:00 +0200

    fetched api

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,19 @@
+import { useEffect,useState} from "react";
+
 function Products(){
+    const [products,setProducts] = useState([])
+    const fetchData =()=>{
+    fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => console.log(data));
+    }
+    useEffect(()=>{
+    fetchData()
+    },[])
     return(
+
         <div>
-            Products
+            {/* {products.map(product=>(<li>{}</li>))} */}
         </div>
     )
 }
