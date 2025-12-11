04a9b911a3564415fb371cb0d02616c274c244e7
Darlene Ishimwe <darleneishimwe73@gmail.com>
2025-12-11 12:23:47 +0200

    displaied cards

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,21 +1,28 @@
-import { useEffect,useState} from "react";
-import {ReuseableCard} from "../component/ReuseableCard"
+import { useEffect, useState } from "react";
+import { ReuseableCard } from "../component/ReuseableCard";
+import {SimpleGrid, Box} from "@mantine/core"
 
-function Products(){
-    const [products,setProducts] = useState([])
-    const fetchData =()=>{
-    fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  .then(data => setProducts(data));
-    }
-    useEffect(()=>{
-    fetchData()
-    },[])
-    return(
+function Products() {
+  const [products, setProducts] = useState([]);
+  const fetchData = () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <Box>
+        <SimpleGrid cols={4} mt={-70}>
+      {products.map((product) => (
+        
+     
+      <ReuseableCard />
+      ))}
+    </SimpleGrid>
 
-        <div>
-            <ReuseableCard />
-        </div>
-    )
+    </Box>
+  );
 }
 export default Products;
